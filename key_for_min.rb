250e0557c5621459976df599cb3be8e3569172a0
Nicole Uyigue-Sivek <nicoleusivek@gmail.com>
2020-09-05 04:53:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,9 @@ def key_for_min_value(name_hash)
  min_key = nil 
  min_value = nil 
  name_hash.each do |key, value|
-   if .
+   if min_value == nil
+     min_value = value 
+     min_key = key
    elsif min_value > value
      min_value = value 
      min_key = key
@@ -14,4 +16,5 @@ def key_for_min_value(name_hash)
   puts "#{key}"
 end
 min_key
+end
 end
